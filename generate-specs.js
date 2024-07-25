8bc5f3d0063e8e7bbe32f576fcdce16ab933b115
justinr1234 <justinr1234@gmail.com>
2024-07-25 15:06:51 -0600

    Fixing version and some modifications in yaml file definition (#5)

--- generate-specs.js
+++ generate-specs.js
@@ -1,4 +1,4 @@
-const $RefParser = require('json-schema-ref-parser')
+const $RefParser = require('@apidevtools/json-schema-ref-parser')
 const fs = require('fs')
 
 if (!fs.existsSync('./dist')){
@@ -10,10 +10,12 @@ $RefParser
   .then((schema) => {
     fs.writeFileSync('dist/async_api.json', JSON.stringify(schema, null, 2))
   })
-  .then(() => $RefParser.dereference('./open_api/json_api.yaml'))
-  .then((schema) => {
-    fs.writeFileSync('dist/open_api.json', JSON.stringify(schema, null, 2))
-  })
+  // .then(() => $RefParser.dereference('./open_api/json_api.yaml'))
+  // .then((schema) => {
+  //   // Deleting the licence key as it is not needed for codegenerators
+  //   delete schema?.info?.license;
+  //   fs.writeFileSync('dist/open_api.json', JSON.stringify(schema, null, 2))
+  // })
   .catch((err) => {
     console.error(err)
   }).finally(() => {
